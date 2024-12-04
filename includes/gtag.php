7f4a23e10bff4a9dddfd7ae0d89bea4909ed8432
Spectrum3900 <jeromeheuze@gmail.com>
2024-12-03 16:42:10 -0800

    Update gtag.php

--- includes/gtag.php
+++ includes/gtag.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-CSQVBW97D1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RC1KXKC4X3"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-CSQVBW97D1');
+    gtag('config', 'G-RC1KXKC4X3');
 </script>

b918ff24ae9bb8aa3132747f8cbaae538fbc7230
Spectrum3900 <jeromeheuze@gmail.com>
2025-01-29 11:55:35 -0800

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,61 @@
     <?php include "./includes/gtag.php" ?>
 </head>
 <body>
-
+<!-- ==== MAIN CONTENT ===== -->
+<div class="site-features border-top space-1d625">
+    <div class="container">
+        <ul class="list-unstyled my-0 list-features d-flex align-items-center justify-content-xl-between overflow-auto overflow-xl-visible">
+            <li class="flex-shrink-0 flex-xl-shrink-1 list-feature">
+                <div class="media">
+                    <div class="feature__icon font-size-14 text-primary-green text-lh-xs">
+                        <i class="glyph-icon flaticon-delivery"></i>
+                    </div>
+                    <div class="media-body ml-4">
+                        <h4 class="feature__title font-size-3">100% Delivery</h4>
+                        <p class="feature__subtitle m-0">Anytime access</p>
+                    </div>
+                </div>
+            </li>
+            <li class="flex-shrink-0 flex-xl-shrink-1 separator mx-4 mx-xl-0 border-left h-fixed-80" aria-hidden="true" role="presentation"></li>
+            <li class="flex-shrink-0 flex-xl-shrink-1 list-feature">
+                <div class="media">
+                    <div class="feature__icon font-size-14 text-primary-green text-lh-xs">
+                        <i class="glyph-icon flaticon-credit"></i>
+                    </div>
+                    <div class="media-body ml-4">
+                        <h4 class="feature__title font-size-3">No Payment</h4>
+                        <p class="feature__subtitle m-0">100% Free Content</p>
+                    </div>
+                </div>
+            </li>
+            <li class="flex-shrink-0 flex-xl-shrink-1 separator mx-4 mx-xl-0 border-left h-fixed-80" aria-hidden="true" role="presentation"></li>
+            <li class="flex-shrink-0 flex-xl-shrink-1 list-feature">
+                <div class="media">
+                    <div class="feature__icon font-size-14 text-primary-green text-lh-xs">
+                        <i class="glyph-icon flaticon-warranty"></i>
+                    </div>
+                    <div class="media-body ml-4">
+                        <h4 class="feature__title font-size-3">Quality Guarantee</h4>
+                        <p class="feature__subtitle m-0">Content That Delivers Results</p>
+                    </div>
+                </div>
+            </li>
+            <li class="flex-shrink-0 flex-xl-shrink-1 separator mx-4 mx-xl-0 border-left h-fixed-80" aria-hidden="true" role="presentation"></li>
+            <li class="flex-shrink-0 flex-xl-shrink-1 list-feature">
+                <div class="media">
+                    <div class="feature__icon font-size-14 text-primary-green text-lh-xs">
+                        <i class="glyph-icon flaticon-help"></i>
+                    </div>
+                    <div class="media-body ml-4">
+                        <h4 class="feature__title font-size-3">24/7 Support</h4>
+                        <p class="feature__subtitle m-0">Contact Us Anytime</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+<!-- ===== END MAIN CONTENT ===== -->
 <?php include "./components/footer.php" ?>
 
 <?php include "./includes/global_footer.php" ?>
